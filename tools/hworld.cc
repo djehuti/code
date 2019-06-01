acad7a84287b624ebe63e0daddac8bc7c61c2c6d
Ben Cox <cox@djehuti.com>
2019-06-01 11:44:14 -0400

    Add hworld example.

---
+++ tools/hworld.cc
@@ -0,0 +1,9 @@
+// Copyright (c) 2019 Ben Cox <cox@djehuti.com>. All Rights Reserved.
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int, char *[]) {
+    std::cout << "Hello, world!" << std::endl;
+    return EXIT_SUCCESS;
+}
